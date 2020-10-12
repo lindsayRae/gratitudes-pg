8d06eeffb1f90202504beaf3e4fe3fb410ed561b
lindsayRae <lbarnett712@gmail.com>
2020-10-11 20:12:54 -0400

    feat - added bootstrap, started new layout

---
+++ client/src/App/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+
+import gratNavbar from './Header.css';
+
+function HeaderRow() {
+  //  const getCurrentDate = new Date().toLocaleDateString;
+
+  return (
+        <nav className="gratNavbar">
+            <div>Sunday October 11th, 2020</div>
+            <FontAwesomeIcon icon={faSignOutAlt} />
+        </nav>
+  );
+}
+
+export default HeaderRow;
--- client/src/App/pages/GratitudeList.js
+++ client/src/App/pages/GratitudeList.js
@@ -39,13 +39,26 @@ function GratitudeList() {
   };
 
   return (
-        <div>
-            <h1>What are you thankful for today?</h1>
-            <GratitudeForm onSubmit={addGrat}/>
-            <Gratitude grats={grats}
-                completeGrat={completeGrat}
-                removeGrat={removeGrat}
-                updateGrat={updateGrat}/>
+        <div style={{ paddingTop: '6rem' }} className='container'>
+          <div className='row justify-content-center'>
+            <div className='col-lg-5'>
+              <div className='bg-white pb-5 card-header'>
+                  <div className='text-center mt-5'>
+                    <h3>Good evening <span>Lindsay</span></h3>
+                    <h5>What are you thankful for today?</h5>
+                  </div>
+              </div>
+              <div className='shadow border-0 card'>
+                <div className='px-lg-5 py-lg-5 card-body'>
+                  <GratitudeForm onSubmit={addGrat}/>
+                  <Gratitude grats={grats}
+                      completeGrat={completeGrat}
+                      removeGrat={removeGrat}
+                      updateGrat={updateGrat}/>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
   );
 }
