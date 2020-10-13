418af3dbee4bd7282c9836611cf5fb79581d3f70
lindsayRae <lbarnett712@gmail.com>
2020-10-12 23:04:18 -0400

    feat - set up daily quote UI

--- client/src/App/pages/DailyQuote/DailyQuote.js
+++ client/src/App/pages/DailyQuote/DailyQuote.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import quoteContainer from './DailyQuote.css';
+import React, { useState, useEffect } from 'react';
+import './DailyQuote.css';
 
-const quoteData = {
-  q: 'Even though you are on the right track - you will get run over if you just sit there.',
-  a: 'Will Rogers',
-  h: '<blockquote>&ldquo;Even though you are on the right track - you will get run over if you just sit there.&rdquo; &mdash; <footer>Will Rogers</footer></blockquote>',
-};
+const mockQuote = [
+  {
+    q: 'Even though you are on the right track - you will get run over if you just sit there.',
+    a: 'Will Rogers',
+    h: '<blockquote>&ldquo;Even though you are on the right track - you will get run over if you just sit there.&rdquo; &mdash; <footer>Will Rogers</footer></blockquote>',
+  },
+];
 
 export default () => {
-  const quote = quoteData.q
-  const author = quoteData.a
+  const [quote, setQuote] = useState([]);
 
-  console.log('quote: ', quote)
-  return(
+  useEffect(() => {
+    const getQuote = async () => {
+      // const res = await fetch('https://zenquotes.io/api/random');
+      // const data = await res.json();
+      console.log(mockQuote);
+      setQuote(mockQuote);
+    };
+    getQuote();
+  }, []);
+
+  return (
     <div className='quoteContainer'>
-    <div>{quote}</div>
+    <div>{mockQuote[0].q}</div>
     <hr></hr>
-    <div>{author}</div>
+    <div>{mockQuote[0].a}</div>
+
   </div>
-  )
-      
+  );
 };
