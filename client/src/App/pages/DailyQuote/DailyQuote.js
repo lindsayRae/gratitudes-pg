41c15dd09716ccee922c0de9d046686535fda11f
lindsayRae <lbarnett712@gmail.com>
2020-10-16 07:16:40 -0400

    feat - added 3rd party API

--- client/src/App/pages/DailyQuote/DailyQuote.js
+++ client/src/App/pages/DailyQuote/DailyQuote.js
@@ -1,33 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import './DailyQuote.css';
 
-const mockQuote = [
-  {
-    q: 'Even though you are on the right track - you will get run over if you just sit there.',
-    a: 'Will Rogers',
-    h: '<blockquote>&ldquo;Even though you are on the right track - you will get run over if you just sit there.&rdquo; &mdash; <footer>Will Rogers</footer></blockquote>',
-  },
-];
-
-export default () => {
-  const [quote, setQuote] = useState([]);
+const usePosts = () => {
+  const [quoteRes, setQuote] = useState([]);
 
   useEffect(() => {
-    const getQuote = async () => {
-      // const res = await fetch('https://zenquotes.io/api/random');
-      // const data = await res.json();
-      console.log(mockQuote);
-      setQuote(mockQuote);
+    const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+    const quoteURL = 'https://zenquotes.io/api/today';
+    const fetchQuote = async () => {
+      const res = await fetch(proxyurl + quoteURL);
+      const data = await res.json();
+      setQuote(data);
     };
-    getQuote();
+    fetchQuote();
   }, []);
+  return quoteRes;
+};
 
+export default () => {
+  const quote = usePosts();
+  console.log(quote);
   return (
     <div className='quoteContainer'>
-    <div>{mockQuote[0].q}</div>
-    <hr></hr>
-    <div>{mockQuote[0].a}</div>
-
+   {quote[0]
+      && <>
+        <div>"{quote[0].q}"</div>
+        <hr></hr>
+        <div>- {quote[0].a}</div>
+      </>
+   }
   </div>
   );
 };
