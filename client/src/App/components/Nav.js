7ceb1321284b8d7407d4e3a9429034314059db45
lindsayRae <lbarnett712@gmail.com>
2020-10-18 10:34:28 -0400

    feat - added login page

--- client/src/App/components/Nav.js
+++ client/src/App/components/Nav.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { NavLink } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { UserContext } from '../context/UserContext';
 
-export default () => (
-    <div className='Nav'>
-       <NavLink to="/" exact>Home</NavLink>
-       <NavLink to="/all" exact>View All Entries</NavLink>
-       <NavLink to="/all" exact>Sign Out <FontAwesomeIcon icon={faSignOutAlt} /></NavLink>
-        <NavLink to="/login" exact>Login</NavLink>
-    </div>
-);
+export default () => {
+  const { user } = useContext(UserContext);
+
+  return (
+        <div className='Nav'>
+        <NavLink to="/" exact>Home</NavLink>
+        {user
+        && <>
+            <NavLink to="/all" exact>View All Entries</NavLink>
+            <NavLink to="/all" exact>Sign Out <FontAwesomeIcon icon={faSignOutAlt} /></NavLink>
+        </>
+        }
+        {!user
+            && <NavLink to="/login" exact>Login</NavLink>
+        }
+
+        </div>
+  );
+};
