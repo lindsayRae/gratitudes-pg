7b7e17ccb95852adf51f863540ff9324bead430a
lindsayRae <lbarnett712@gmail.com>
2020-10-19 21:16:35 -0400

    -feat added bootstrap buttons

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -10,17 +10,6 @@ export default () => {
   const { user, setUser } = useContext(UserContext);
   const today = new Date();
 
-  // function greeting() {
-  //   const { hours } = new Date();
-
-  //   console.log(user);
-  //   if (hours > 12 && hours < 17) {
-  //     return 'Good Afternoon, Friend';
-  //   } if (hours > 17) {
-  //     return 'Good Evening, Friend';
-  //   }
-  //   return 'Good Morning, Friend';
-  // }
   return (
 
       <div className="pt-lg-7 container">
@@ -28,7 +17,7 @@ export default () => {
           <div className="col-lg-5">
             <div className="bg-secondary shadow border-0 card">
               <div className="bg-white pb-4 pt-4 card-header text-center">
-              <p>{`${today.getMonth() + 1}-${today.getDate()}-${today.getFullYear()}`}</p>
+              <p className='text-right'>{`${today.getMonth() + 1}-${today.getDate()}-${today.getFullYear()}`}</p>
                 <Greeting />
                 <h5>What are you thankful for today?</h5>
               </div>
--- client/src/App/pages/PostedGrats/PostedGrats.js
+++ client/src/App/pages/PostedGrats/PostedGrats.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 export default ({ id, date, dailyGratitude }) => (
       <section className='section section-lg pt-lg-0 mt--200'>
@@ -6,13 +7,13 @@ export default ({ id, date, dailyGratitude }) => (
                 <div className='row justify-content-center'>
                     <div className='col-lg-12'>
                         <div className='row-grid row'>
-                            <div className='col-lg-4'>
-                                <div className='card shadow border-0'>
+                            <div className='col-lg-4 col-md-5 col-sm-6'>
+                                <div className='card shadow border-0 mb-4'>
                                     <div className='py-5 card-body'>
                                         <h6>{date}</h6>
                                         <p className='description'>{dailyGratitude}</p>
-                                        <button className='btn'>Delete</button>
-                                        <button className='btn'>Edit</button>
+                                        <Button type='button' variant="danger" size="sm" className='btn'>Delete</Button>
+                                        <Button type='button' variant="info" size="sm" className='btn ml-3'>Edit</Button>
                                     </div>
                                 </div>
                             </div>
