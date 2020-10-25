055bfa996a7ccf7a7a0f473564fc418da46993c6
lindsayRae <lbarnett712@gmail.com>
2020-10-25 11:22:04 -0400

    feat - started authentication

--- client/src/App/context/UserContext.js
+++ client/src/App/context/UserContext.js
@@ -1,9 +1,21 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 export const UserContext = createContext(null);
 
 export default ({ children }) => {
   const [user, setUser] = useState(null);
+
+  // const login = (email) => {
+  // };
+
+  const fetchUser = async () => {
+    fetch('/api/me');
+    return user;
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
   return (
       <UserContext.Provider value={{ user, setUser }}>
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 const gratitudes = require('./gratitudes');
+const auth = require('./auth');
 
 module.exports = router;
 
 router.use('/gratitudes', gratitudes);
+router.use('/auth', auth);
