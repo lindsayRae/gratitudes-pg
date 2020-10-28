0754a3edd1ac96c02dae764e67012b3ff7183749
lindsayRae <lbarnett712@gmail.com>
2020-10-28 08:12:19 -0400

    chore - comment cleanup

--- client/src/App/pages/AllGratitudes.js
+++ client/src/App/pages/AllGratitudes.js
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PostedGrats from './PostedGrats/PostedGrats';
 
-console.log('from file');
 // follow this pattern
 const usePosts = () => {
   const [posts, setPosts] = useState([]);
 
   // passing the [] says only run once -> the the compnenet mounts
-  // use effect is called when component mounts 
+  // use effect is called when component mounts
   useEffect(() => {
-    console.log('use effect was called')
+    console.log('use effect was called');
     const getGrats = async () => {
       const res = await fetch('http://localhost:5000/api/gratitudes');
       const data = await res.json();
@@ -23,7 +22,7 @@ const usePosts = () => {
 };
 export default () => {
   const posts = usePosts();
-  console.log('from render method posts', posts);
+  console.log('posts', posts);
   return (
     <div className='container'>
       {
--- client/src/App/pages/PostedGrats/PostedGrats.js
+++ client/src/App/pages/PostedGrats/PostedGrats.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 
-export default (props) => {
-  //console.log(props);
-  return (
-      <section className='section section-lg pt-lg-0 mt--200'>
-            <div className='container'>
-                <div className='row justify-content-center'>
-                    <div className='col-lg-12'>
-                        <div className='row-grid row'>
-                            <div className='col-lg-4 col-md-5 col-sm-6'>
-                                <div className='card shadow border-0 mb-4'>
-                                    <div className='py-5 card-body'>
-                                        <h6>My Gratitude for {props.date}</h6>
-                                        <p className='description'>{props.dailyGratitude}</p>
-
-                                    </div>
+export default (props) => (
+    <section className='section section-lg pt-lg-0 mt--200'>
+        <div className='container'>
+            <div className='row justify-content-center'>
+                <div className='col-lg-12'>
+                    <div className='row-grid row'>
+                        <div className='col-lg-4 col-md-5 col-sm-6'>
+                            <div className='card shadow border-0 mb-4'>
+                                <div className='py-5 card-body'>
+                                    <h6>My Gratitude for {props.date}</h6>
+                                    <p className='description'>{props.dailyGratitude}</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-      </section>
-  );
-};
+        </div>
+    </section>
+);
